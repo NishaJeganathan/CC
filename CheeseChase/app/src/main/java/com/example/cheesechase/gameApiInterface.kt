9de72a2dd208a330f0ec4793afae438ebbee93cd
NishaJeganathan <jeganathannisha1@gmail.com>
2024-07-05 23:23:42 +0530

    commit

--- CheeseChase/app/src/main/java/com/example/cheesechase/gameApiInterface.kt
+++ CheeseChase/app/src/main/java/com/example/cheesechase/gameApiInterface.kt
@@ -9,8 +9,6 @@ interface gameApiInterface {
 
     @GET("/obstacleLimit")
     fun getObstacleLimit(): Call<obstacleLimitResponse>
-    @GET("/image")
-    fun getImage(@Query("character") character:String):Call<responseBody>
 }
 
 data class obstacleLimitResponse(val obstacleLimit:Int)
